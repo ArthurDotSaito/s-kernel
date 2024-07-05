d67fc421f0359ce1863c78831a99329bc8493f0a
Arthur Saito <arthur_saito1998@hotmail.com>
2024-07-05 00:35:19 -0300

    rename kernel

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,7 +1,7 @@
 [BITS 32]
 
 global _start
-extern kernel_start
+extern kernel_main
 
 CODE_SEG equ 0x08
 DATA_SEG equ 0x10
@@ -21,8 +21,8 @@ _start:
     or al, 2
     out 0x92, al
 
-    call kernel_start
-    
+    call kernel_main
+
     jmp $
 
 times 512 - ($ - $$) db 0
