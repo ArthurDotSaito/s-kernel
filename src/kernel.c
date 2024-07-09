5d0678a9c0a959cb99e9a4b5aadcd0c7c78c9102
Arthur Saito <arthur_saito1998@hotmail.com>
2024-07-08 23:29:10 -0300

    now, i can print ASCII chars on screen

--- src/kernel.c
+++ src/kernel.c
@@ -3,10 +3,28 @@
 #include <stddef.h>
 
 uint16_t *video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
 uint16_t terminal_make_char(char c, char color)
 {
     return color << 8 | c;
+}
+
+void terminal_putchar(int x, int y, char c, char color)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
+}
+
+void terminal_writechar(char c, char color)
+{
+    terminal_putchar(terminal_col, terminal_row, c, color);
+    terminal_col += 1;
+    if (terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
 }
 
 void terminal_initialize()
@@ -14,9 +32,9 @@ void terminal_initialize()
     video_mem = (uint16_t *)(0xb8000);
     for (int y = 0; y < VGA_HEIGHT; y++)
     {
-        for (int x = 0; x < VGA_HEIGHT; x++)
+        for (int x = 0; x < VGA_WIDTH; x++)
         {
-            video_mem[(y * VGA_WIDTH + x)] = terminal_make_char(' ', 0);
+            terminal_putchar(x, y, ' ', 0);
         }
     }
 }
@@ -35,5 +53,8 @@ size_t strlen(const char *str)
 void kernel_main()
 {
     terminal_initialize();
-    video_mem[0] = terminal_make_char('B', 15);
+    terminal_row = 0;
+    terminal_col = 0;
+    terminal_writechar('A', 15);
+    terminal_writechar('B', 15);
 }
