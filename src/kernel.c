e170068322e2252075d8f8fe2143895ed9545120
Arthur Saito <arthur_saito1998@hotmail.com>
2024-07-08 23:06:46 -0300

    cleaning screen with ASCII

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,26 @@
 #include "kernel.h"
-#include <stdint.h>
+
+uint16_t *video_mem = 0;
+
+uint16_t terminal_make_char(char c, char color)
+{
+    return color << 8 | c;
+}
+
+void terminal_initialize()
+{
+    video_mem = (uint16_t *)(0xb8000);
+    for (int y = 0; y < VGA_HEIGHT; y++)
+    {
+        for (int x = 0; x < VGA_HEIGHT; x++)
+        {
+            video_mem[(y * VGA_WIDTH + x)] = terminal_make_char(' ', 0);
+        }
+    }
+}
 
 void kernel_main()
 {
-    uint16_t *video_mem = (uint16_t *)(0xb8000);
-    video_mem[0] = 0x341;
+    terminal_initialize();
+    video_mem[0] = terminal_make_char('B', 4);
 }
