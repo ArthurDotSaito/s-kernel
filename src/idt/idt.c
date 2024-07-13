58bd09a0b97e150eeab6cdd4a4a72a4ed49bb537
Arthur Saito <arthur_saito1998@hotmail.com>
2024-07-13 18:16:52 -0300

    + interruption zero

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -5,6 +5,11 @@
 struct idt_desc idt_descriptors[SOS_TOTAL_INTERRUPTS];
 struct idtr_desc idtr_descriptor;
 
+void idt_zero()
+{
+    print("Division by zero error\n");
+}
+
 void idt_set(int interrupt_no, void *address)
 {
     struct idt_desc *desc = &idt_descriptors[interrupt_no];
@@ -20,4 +25,5 @@ void idt_init()
     memset(idt_descriptors, 0, sizeof(idt_descriptors));
     idtr_descriptor.limit = sizeof(idt_descriptors) - 1;
     idtr_descriptor.base = idt_descriptors;
+    idt_set(0, idt_zero)
 }
