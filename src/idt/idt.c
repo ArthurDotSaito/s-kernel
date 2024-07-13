8ecc2a3562255400e5219d6c6a8ca5a8732fc884
Arthur Saito <arthur_saito1998@hotmail.com>
2024-07-13 18:15:39 -0300

    ++setting idt

--- src/idt/idt.c
+++ src/idt/idt.c
@@ -11,6 +11,8 @@ void idt_set(int interrupt_no, void *address)
     desc->offset_1 = (int32_t)address & 0x0000ffff;
     desc->selector = KERNEL_CODE_SELECTOR;
     desc->zero = 0x00;
+    desc->type_attr = 0xEE;
+    desc->offset_2 = (int32_t)address >> 16;
 }
 
 void idt_init()
