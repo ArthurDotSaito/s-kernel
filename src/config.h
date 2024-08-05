cd3669bd176bef9b0ceafa692b82bfc1cb6eabab
Arthur Saito <arthur_saito1998@hotmail.com>
2024-08-04 22:03:24 -0300

    ++ heap boilerplate

--- src/config.h
+++ src/config.h
@@ -5,7 +5,10 @@
 #define KERNEL_CODE_SELECTOR 0x08
 #define KERNEL_DATA_SELECTOR 0x10
 
-// This should be enough for 4096 byte entry.
+// This should be enough for 4096 byte entry ~100Mb heap size.
 #define SOS_HEAP_SIZE_BYTES 104857600
+#define SOS_HEAP_BLOCK_SIZE 4096
+#define SOS_HEAP_ADDRESS 0x01000000
+#define SOS_HEAP_TABLE_ADDRESS 0x00007E00
 
 #endif
--- src/memory/heap/kheap.c
+++ src/memory/heap/kheap.c
@@ -1,5 +1,7 @@
 #include "kheap.h"
 #include "heap.h"
+#include "config.h"
+#include "kernel.h"
 
 struct heap kernel_heap;
 
@@ -7,4 +9,14 @@ struct heap_table kernel_heap_table;
 
 void kheap_init()
 {
+    int total_table_entries = SOS_HEAP_SIZE_BYTES / SOS_HEAP_BLOCK_SIZE;
+    kernel_heap_table.entries = (HEAP_BLOCK_TABLE_ENTRY *)(SOS_HEAP_TABLE_ADDRESS);
+    kernel_heap_table.total = total_table_entries;
+
+    void *end = (void *)(SOS_HEAP_ADDRESS + SOS_HEAP_SIZE_BYTES);
+    int res = heap_create(&kernel_heap, (void *)(SOS_HEAP_ADDRESS), end, &kernel_heap_table);
+    if (res < 0)
+    {
+        print("Panic! Failed to create heap!\n");
+    }
 }
