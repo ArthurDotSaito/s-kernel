cf20dd86c3ffd7a50224d6016e0aab3c72db58b9
Arthur Saito <arthur_saito1998@hotmail.com>
2024-08-05 23:14:15 -0300

    validating heap nonalignment

--- src/memory/heap/heap.c
+++ src/memory/heap/heap.c
@@ -1,4 +1,12 @@
 #include "heap.h"
+#include "kernel.h"
+#include <stdbool.h>
+#include "status.h"
+
+static int heap_validate_alignment(void *ptr)
+{
+    return ((unsigned int)ptr % SOS_HEAP_BLOCK_SIZE) == 0;
+}
 
 /*
 When I want to create a new heap, I should pass a blank heap struct, a pointer to start address
@@ -7,5 +15,14 @@ a poiter to the most right address and a valid heap table.
 
 int heap_create(struct heap *heap, void *ptr, void *end, struct heap_table *table)
 {
-    return 0;
+    int res = 0;
+
+    if (!heap_validate_alignment(ptr) || !heap_validate_alignment(end))
+    {
+        res = -EINVARG;
+        goto out;
+    }
+
+out:
+    return res;
 }
