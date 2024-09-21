b2d7da9afba51b6e13fac1ed7c68c92b4b432c51
Arthur Saito <arthur_saito1998@hotmail.com>
2024-09-20 21:30:27 -0300

    typos fix

--- src/memory/heap/heap.h
+++ src/memory/heap/heap.h
@@ -2,11 +2,11 @@
 #define HEAP_H
 
 #include "config.h"
-#include "stdint.h"
-#include "stddef.h"
+#include <stdint.h>
+#include <stddef.h>
 
 #define HEAP_BLOCK_TABLE_ENTRY_TAKEN 0x01
-#define HEAP_BLOCK_TABLE_ENTRY_FREE 0X00
+#define HEAP_BLOCK_TABLE_ENTRY_FREE 0x00
 
 #define HEAP_BLOCK_HAS_NEXT 0b10000000
 #define HEAP_BLOCK_IS_FIRST 0b010000000
